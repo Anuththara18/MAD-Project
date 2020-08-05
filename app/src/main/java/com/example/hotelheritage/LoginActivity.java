a3bd020a0f0d68536ff2f4371246eabfd3252c54
Anuththara18 <anuththarajayakody18@gmail.com>
2020-08-05 18:31:15 +0530

    Login Page Updated

---
+++ app/src/main/java/com/example/hotelheritage/LoginActivity.java
@@ -0,0 +1,121 @@
+package com.example.hotelheritage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button abutton;
+
+    EditText email;
+    EditText password;
+    Button signIn;
+    Button signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        signIn = findViewById(R.id.signIn);
+        signUp = findViewById(R.id.signUp);
+
+    /*
+        abutton = findViewById(R.id.abutton);
+        abutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent img = new Intent(getApplicationContext(), RoomsSelectionActivity.class);
+                startActivity(img);
+
+            }
+        });
+    */
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                checkDataEntered();
+            }
+        });
+
+        /*
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUp = new Intent(getApplicationContext(), Sign_Up.class);
+                startActivity(signUp);
+            }
+        });
+         */
+    }
+
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    void checkDataEntered() {
+
+        boolean isValid = true;
+        if (isEmpty(email)) {
+            Toast t = Toast.makeText(this, "You must enter email to register!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+
+        else {
+            if (!isEmail(email)) {
+                email.setError("Enter valid email!");
+                isValid = false;
+            }
+        }
+
+        if (isEmpty(password)) {
+            password.setError("Password is required!");
+            isValid = false;
+        }
+        else {
+            if ( password.getText().toString().length() < 4 ) {
+                password.setError("Password must be at least 4 characters long!");
+                isValid = false;
+            }
+        }
+
+        /*//check email and password
+        //IMPORTANT: here should be call to backend or safer function for local check; For example simple check is cool
+        //For example simple check is cool
+        if (isValid) {
+            String usernameValue = username.getText().toString();
+            String passwordValue = password.getText().toString();
+            if (usernameValue.equals("test@test.com") && passwordValue.equals("password1234")) {
+                //everything checked we open new activity
+                Intent i = new Intent(LoginActivity.this, FirstActivity.class);
+                startActivity(i);
+                //we close this activity
+                this.finish();
+            } else {
+                Toast t = Toast.makeText(this, "Wrong email or password!", Toast.LENGTH_SHORT);
+                t.show();
+            }
+        }
+
+         */
+    }
+
+}
--- app/src/main/java/com/example/hotelheritage/MainActivity.java
+++ app/src/main/java/com/example/hotelheritage/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.hotelheritage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button lg = findViewById(R.id.lgBtn);
+
+        lg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent lga = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(lga);
+            }
+        });
+
     }
 }
 
