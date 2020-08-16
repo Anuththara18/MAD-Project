52853093270c0553a6b372a09ecf27939cfab6de
dilip vijethunga <dilip.vijethunga@gmail.com>
2020-08-16 19:02:23 +0530

    three sections

--- app/src/main/java/com/example/hotelheritage/ActivitesHome.java
+++ app/src/main/java/com/example/hotelheritage/ActivitesHome.java
@@ -2,13 +2,27 @@ package com.example.hotelheritage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivitesHome extends AppCompatActivity {
-
+    public Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activites_home);
+
+        button = (Button) findViewById(R.id.gym);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivitesHome.this, GymSection.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/hotelheritage/GymSection.java
@@ -0,0 +1,17 @@
+package com.example.hotelheritage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class GymSection extends AppCompatActivity {
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gym_section);
+
+    }
+}
